f6365a08568ac7809c03b3695ec7b0c75c5f9a07
victormarius <100percentfragrance@gmail.com>
2022-03-17 16:44:01 +0100

    deploying cloud run app

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import os
+
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def hello_world():
-    return 'Hello, Docker!'
+    return "Hello YouTube!! I hope you liked it"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
